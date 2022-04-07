6b0e1de1c90df8f60adead6cf2647fb175e9ca17
Keerthana.So <Keerthana.So@EXIND3010-77.allfon.in>
2022-04-07 11:59:11 +0530

    Fifth Commit

---
+++ revision/src/com/revision/Refresh.java
@@ -0,0 +1,13 @@
+package com.revision;
+
+public class Refresh{
+      String name;
+	public static void main(String[] args) {
+		Refresh R1 = new Refresh();
+		
+   System.out.println("hi");
+   System.out.println(R1.name); 
+  
+	}
+
+}
